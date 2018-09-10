eb793146fb4d8978a083ec2a72b795dfe0f4562f
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-09 18:05:30 -0700

    added few functions.

---
+++ Spaceman.py
@@ -0,0 +1,28 @@
+import random
+
+lines = open('/usr/share/dict/words', 'rU').read().splitlines()
+# print(lines)
+
+# This will be the varible that will chose a random word for my user to guess.
+secret_word = random.choice(lines)
+
+print(secret_word)
+
+#This keeps track of how many times my users can incorrectly guess.
+wrong_guess = len(secret_word)
+right_guess = list()
+
+# it loops through the random word and each time it does it adds a '-' for each letterself.
+for i in range(len(secret_word)-1):
+
+# This line is appending the '-' the right guess list!
+    right_guess.append('-')
+# This asks the user for an input letter.
+
+guess = raw_input("Enter a letter: ")
+# print(type(guess))
+
+if guess in secret_word:
+    print("yayyyyy")
+else:
+    print("Try again")

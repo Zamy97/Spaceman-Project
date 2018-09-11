7813d2c0ed36370989f03ba01c1836dae0399f08
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-11 13:07:15 -0700

    on progress

--- Spaceman.py
+++ Spaceman.py
@@ -9,38 +9,55 @@ secret_word = random.choice(lines)
 print(secret_word)
 
 dashes = "-" * len(secret_word)
-dashes_left = 7
+dashes_left = len(dashes)
 
-while dashes_left >-1 and not  dashes == secret_word:
+while dashes_left >=1:
     print(dashes)
     print(str(dashes_left))
 
     guess = input("Enter a letter: ")
 
-    if len(guess) != 1:
-        print("please type one letter")
-
-    elif guess in secret_word:
-        print("It is in the word")
-        dashes = update_dashes(secret_word, dashes, guess)
-
-    else:
-        print("The letter is not in secret woed. Try again.")
-        dashes_left -= 1
-
-    if dashes_left < 0:
-        print("you lost")
-    else:
-        print("you win")
+    if guess in secret_word:
 
 
-def update_dashes(secret, cur_dash, rec_dash):
-    result = ""
 
-    for i in range(len(secret)):
-        if secret[i] == rec_dash:
-            result = result + rec_dash
 
-        else:
-            result = result + cur_dash[i]
-    return result
+
+
+
+
+
+
+
+
+
+
+
+
+#     if len(guess) != 1:
+#         print("please type one letter")
+#
+#     elif guess in secret_word:
+#         print("It is in the word")
+#         dashes = update_dashes(secret_word, dashes, guess)
+#
+#     else:
+#         print("The letter is not in secret word. Try again.")
+#         dashes_left -= 1
+#
+#  if dashes_left < 0:
+#      print("you lost")
+#  else:
+#      print("you win")
+#
+#
+# def update_dashes(secret, cur_dash, rec_dash):
+#     result = ""
+#
+#     for i in range(len(secret)):
+#         if secret[i] == rec_dash:
+#             result = result + rec_dash
+#
+#          else:
+#              result = result + cur_dash[i]
+#     return result

e2e9d3b37af5b2eeb101f7c5b204954bb96fa73b
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-11 12:00:28 -0700

    still not working

--- Spaceman.py
+++ Spaceman.py
@@ -9,7 +9,7 @@ secret_word = random.choice(lines)
 print(secret_word)
 
 dashes = "-" * len(secret_word)
-dashes_left = len(secret_word)
+dashes_left = 7
 
 while dashes_left >-1 and not  dashes == secret_word:
     print(dashes)
@@ -19,6 +19,28 @@ while dashes_left >-1 and not  dashes == secret_word:
 
     if len(guess) != 1:
         print("please type one letter")
+
     elif guess in secret_word:
         print("It is in the word")
         dashes = update_dashes(secret_word, dashes, guess)
+
+    else:
+        print("The letter is not in secret woed. Try again.")
+        dashes_left -= 1
+
+    if dashes_left < 0:
+        print("you lost")
+    else:
+        print("you win")
+
+
+def update_dashes(secret, cur_dash, rec_dash):
+    result = ""
+
+    for i in range(len(secret)):
+        if secret[i] == rec_dash:
+            result = result + rec_dash
+
+        else:
+            result = result + cur_dash[i]
+    return result

146bdda793dc93aa18bbd3d708527b0c2272caea
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-11 11:37:42 -0700

    added a fe function

--- Spaceman.py
+++ Spaceman.py
@@ -1,6 +1,6 @@
 import random
 
-lines = open('/usr/share/dict/words', 'rU').read().splitlines()
+lines = open('/usr/share/dict/words', 'r').read().splitlines()
 # print(lines)
 
 # This will be the varible that will chose a random word for my user to guess.
@@ -9,3 +9,16 @@ secret_word = random.choice(lines)
 print(secret_word)
 
 dashes = "-" * len(secret_word)
+dashes_left = len(secret_word)
+
+while dashes_left >-1 and not  dashes == secret_word:
+    print(dashes)
+    print(str(dashes_left))
+
+    guess = input("Enter a letter: ")
+
+    if len(guess) != 1:
+        print("please type one letter")
+    elif guess in secret_word:
+        print("It is in the word")
+        dashes = update_dashes(secret_word, dashes, guess)
---
+++ spaceman-1.py
@@ -0,0 +1,53 @@
+import random
+
+lines = open('/usr/share/dict/words', 'rU').read().splitlines()
+# print(lines)
+
+# This will be the varible that will chose a random word for my user to guess.
+secret_word = random.choice(lines)
+
+print(secret_word)
+
+#This keeps track of how many times users can incorrectly guess.
+wrong_guess = len(secret_word)
+print(wrong_guess)
+# it's a string - representation with each letter reveled with being a correct guess.
+right_guess = list()
+
+# it loops through the random word and each time it does it adds a '-' for each letterself.
+for i in range(len(secret_word)-1):
+
+# This line is appending the '-' the right guess list!
+    right_guess.append('-')
+
+
+# This asks the user for an input letter.
+guess = raw_input("Enter a letter: ")
+
+
+# print(type(guess))
+
+
+
+if guess in secret_word:
+     print("yayyyyy")
+
+else:
+    print("Try again")
+
+while wrong_guess > 0:
+
+    for letter in range(len(secret_word)-1):
+
+        if guess == secret_word[letter]:
+            right_guess[letter]= guess
+        else:
+
+            wrong_guess -=1
+
+            print(right_guess)
+
+if wrong_guess == len(secret_word):
+    print("you lost")
+else:
+    print("you won! Yaaay")
